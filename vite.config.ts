7f4d822934f68bf42495d5847b035fea5637cac4
Sathwik Gurugubelli <gurugubellisathwik@gmail.com>
2025-08-04 22:46:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/sathwiks-portfolio-showcase/", // Add this line!
   server: {
     host: "::",
     port: 8080,
